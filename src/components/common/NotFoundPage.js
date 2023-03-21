7e2a595f197a16f0ec811cd4758c0128fef6fa84
jst0951 <jst0951@gmail.com>
2023-03-21 12:14:24 +0900

    changed wrong router

--- src/components/common/NotFoundPage.js
+++ src/components/common/NotFoundPage.js
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const NotFoundPage = () => {
-  return <div>Not Found Error.(404)</div>;
+  return (
+    <div>
+      Not Found Error.(404)
+      <p>
+        Did you mean <Link to="/crypto/transactionfee/bithumb">https://moneyctrlcv.com/crypto/transactionfee/bithumb</Link>?
+      </p>
+    </div>
+  );
 };
 
 export default NotFoundPage;
